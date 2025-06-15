07cfb1163a1e4d7ef15e53e6d8b012bdba27508c
kashish25052004 <kashishjaiswal25052004@gmail.com>
2025-06-16 01:12:34 +0530

    add user account management routes: change password, get current user, update account details, upload avatar and cover image

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,loginUser,logoutUser,refreshAccessToken } from "../controllers/user.controllers.js";
+import { registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword, getCurrentUser, updateAccountDetails, getUserChannelProfile, updateUserAvatar, updateUserCoverImage, getWatchHistory } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
@@ -25,7 +25,18 @@ router.route("/logout").post(verifyJWT, logoutUser)//here we use verifyJWT middl
 
 router.route("/refresh-Token").post(refreshAccessToken);// here we do not use verifyJWT middleware because we are using refresh token to get new access token
 
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
 
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)// here we use upload.single("avatar") middleware to upload single file with field name "avatar";
+
+
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)// here we use upload.single("coverImage") middleware to upload single file with field name "coverImage";
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 
 export default router;
